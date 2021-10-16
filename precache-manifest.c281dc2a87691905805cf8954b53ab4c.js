9ec99cb9c550749e68f9113d6f8938e1d83acec4
Jonathan <jonathan.mc.96@hotmail.com>
2021-10-16 13:31:26 -0500

    Updates

---
+++ precache-manifest.c281dc2a87691905805cf8954b53ab4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e989155cac23f7915d5deeb15a744fbc",
+    "url": "/Journal-app/index.html"
+  },
+  {
+    "revision": "1cd34e6b70e99cae9159",
+    "url": "/Journal-app/static/css/main.cfe7d028.chunk.css"
+  },
+  {
+    "revision": "ffdff01b3d2584b5404d",
+    "url": "/Journal-app/static/js/2.ac15bc5c.chunk.js"
+  },
+  {
+    "revision": "caf1e2ebc544d77bcca7cc92a0536d75",
+    "url": "/Journal-app/static/js/2.ac15bc5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd34e6b70e99cae9159",
+    "url": "/Journal-app/static/js/main.bcd0e4d5.chunk.js"
+  },
+  {
+    "revision": "05616eabc934f1ee69b8",
+    "url": "/Journal-app/static/js/runtime-main.5d0b86c8.js"
+  }
+]);
